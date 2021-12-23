4e99947dbfe00f196b39a3c12d52a535d4110432
rizkaariftiani <rizkaariftiani@gmail.com>
2021-12-23 23:35:56 +0700

    Update

---
+++ php/src/includes/conf.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', '10.0.0.169');
+define('DB_USER', 'admin');
+define('DB_PASS', 'Ik@nur2341');
+define('DB_NAME', 'newsportal');
+?>
--- php/src/includes/config.php
+++ php/src/includes/config.php
@@ -1,8 +1,4 @@
 <?php
-define('DB_SERVER','172.20.0.2');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','newsportal');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
